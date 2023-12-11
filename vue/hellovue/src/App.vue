23747d1b9a2de3c9b591c70c0e379253cd77a6f5
wuchangqian <wuchangqian@gmail.com>
2023-12-11 11:54:17 +0800

    m

--- vue/hellovue/src/App.vue
+++ vue/hellovue/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <my-nav></my-nav>
+    <!-- <img src="./assets/logo.png"> -->
+    <transition name="fade">
+      <router-view/>
+    </transition>
+    <com-footer></com-footer>
   </div>
 </template>
 
@@ -12,12 +16,17 @@ export default {
 </script>
 
 <style>
+body{
+  padding:0;
+  margin:0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #1776d5;
+  margin-top: 0;
+  padding: 0;
 }
 </style>
--- vue/hellovue/src/main.js
+++ vue/hellovue/src/main.js
@@ -2,14 +2,23 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Nav from './components/common/Nav'
+import Footer from './components/common/Footer'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI);
+Vue.component('my-nav', Nav);
+Vue.component('com-footer', Footer);
+
 /* eslint-disable no-new */
-new Vue({
+const app = new Vue({
   el: '#app',
   router,
   components: { App },
   template: '<App/>'
 })
+// console.log(app);
